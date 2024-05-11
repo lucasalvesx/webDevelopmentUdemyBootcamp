e8530552aadb6f842d6f391305111790f87f7004
Lucas <lucassalves005@gmail.com>
2024-05-11 17:24:14 -0300

    1105241724

--- javascript/features/projectChangingStringCasing.js
+++ javascript/features/projectChangingStringCasing.js
@@ -1,12 +1,18 @@
 // This project creates a prompt requesting the user to input their name, 
 // and automatically capitalize the first letter;
-// Uses the slice and toUpperCase features all by myself.
+// Using slice and toUpperCase features all by myself.
 
 // 1 Create a var to store users name via prompt
 var name = prompt("Insert your name here: ");
 
-// 2 capitalize the first letter
-var firstLetter = name.slice(0,1);
+// 2 isolate the first character of the name from the NAME var by slicing it
+var firstChar = name.slice(0,1);
 
-// Print the result via alert
-alert("Hello, " + firstLetter + name + "! ");
+// 3 after isolating, turn it into upper case by calling a native JS method (attention on syntaxis)
+var upperCaseChar = firstChar.toUpperCase();
+
+// 4 isolate the rest of name which won't be capitalizes
+var restOfName = name.slice(1, name.length);
+
+// Print the result via alert, concatenating the variables
+alert("Hello, " + upperCaseChar + restOfName + "! " );
