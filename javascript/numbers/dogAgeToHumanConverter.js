eddfd56c8c967ccd584dacea3882904c111c374d
Lucas <lucassalves005@gmail.com>
2024-05-11 18:32:07 -0300

    1105241724

--- javascript/numbers/dogAgeToHumanConverter.js
+++ javascript/numbers/dogAgeToHumanConverter.js
@@ -1,2 +1,8 @@
 // formula
-humanAge = (dogAge - 2) * 4 + 21
+// humanAge = (dogAge - 2) * 4 + 21
+
+var dogAge = prompt("How old is your dog?");
+
+var humanAge = (dogAge - 2 ) * 4 + 21;
+
+alert("If your dog was human, it'd be " + humanAge + " years old.");
