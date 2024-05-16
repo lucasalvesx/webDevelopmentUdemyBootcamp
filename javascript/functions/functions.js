1b3e200543b00bd43369e2a246567936672bff48
Lucas <lucassalves005@gmail.com>
2024-05-16 19:58:17 -0300

    16052024

---
+++ javascript/functions/functions.js
@@ -0,0 +1,31 @@
+// Functions are a number of instructions, saved inside one only block of code
+
+function getMilk() {
+    var leaveHouse
+    var moveRight
+    var moveRight
+    var moveUp
+    var moveUp
+    var moveUp
+    var moveUp
+    var moveRight
+    var moveRight
+    var buyMilk
+    var moveLeft
+    var moveLeft
+    var moveDown
+    var moveDown
+    var moveDown
+    var moveDown
+    var moveLeft
+    var moveLeft
+    var enterHouse
+}
+
+// So later we can call it, by simply doing:
+
+getMilk();
+
+// Then all instructions iside the block will be executed
+
+// unrelated side note:CTRL + Shift + L - changes all cursors with same name/ocurrences!!
