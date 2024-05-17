0177e32e3a419dcc3ce9fb408e70386c96c9e2fb
Lucas <lucassalves005@gmail.com>
2024-05-17 06:47:38 -0300

    16052024

--- javascript/functions/functions.js
+++ javascript/functions/functions.js
@@ -1,25 +1,25 @@
 // Functions are a number of instructions, saved inside one only block of code
 
 function getMilk() {
-    var leaveHouse
-    var moveRight
-    var moveRight
-    var moveUp
-    var moveUp
-    var moveUp
-    var moveUp
-    var moveRight
-    var moveRight
-    var buyMilk
-    var moveLeft
-    var moveLeft
-    var moveDown
-    var moveDown
-    var moveDown
-    var moveDown
-    var moveLeft
-    var moveLeft
-    var enterHouse
+    alert("leaveHouse")
+    alert("moveRight")
+    alert("moveRight")
+    alert("moveUp")
+    alert("moveUp")
+    alert("moveUp")
+    alert("moveUp")
+    alert("moveRight")
+    alert("moveRight")
+    alert("buyMilk")
+    alert("moveLeft")
+    alert("moveLeft")
+    alert("moveDown")
+    alert("moveDown")
+    alert("moveDown")
+    alert("moveDown")
+    alert("moveLeft")
+    alert("moveLeft")
+    alert("enterHouse")
 }
 
 // So later we can call it, by simply doing:
@@ -27,5 +27,8 @@ function getMilk() {
 getMilk();
 
 // Then all instructions iside the block will be executed
+
+// syntax is: function name(parameters){ }
+// and when it is received a value to a parameter, we call it an ARGUMENT
 
 // unrelated side note:CTRL + Shift + L - changes all cursors with same name/ocurrences!!
---
+++ javascript/functions/getMilk.js
@@ -0,0 +1,37 @@
+// project following the previous example of the getMilkRobot
+// but now, we'll specify the amount of milk bottles we'd like the robot to go buy
+// we do that by adding input values
+
+// we add the name of the desired input, in the function declaration
+// When you pass smthng as an argument in a function, it creates a var
+
+// same function as previous example:
+function getMilk(bottles) {
+    alert ("leaveHouse")
+    alert ("moveRight")
+    alert ("moveRight")
+    alert ("moveUp")
+    alert ("moveUp")
+    alert ("moveUp")
+    alert ("moveUp")
+    alert ("moveRight")
+    alert ("moveRight")
+
+    //adding var input, so when the function gets executed, it executes with the user-selected value
+    alert ("buy" + bottles + "botles of Milk")
+
+    alert ("moveLeft")
+    alert ("moveLeft")
+    alert ("moveDown")
+    alert ("moveDown")
+    alert ("moveDown")
+    alert ("moveDown")
+    alert ("moveLeft")
+    alert ("moveLeft")
+    alert ("enterHouse")
+}
+
+// so to call the function, we do:
+getMilk(2);
+
+// then the variable bottles is = 2
