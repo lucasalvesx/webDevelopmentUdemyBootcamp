630b3929992f91e7bf81e4d69a90b65d0c208aef
Lucas <lucassalves005@gmail.com>
2024-05-20 22:40:19 -0300

    20052024

--- javascript/functions/lifeInWeeks.js
+++ javascript/functions/lifeInWeeks.js
@@ -1,8 +1,9 @@
 // goal is to display text saying how many days, weeks and years of life we have left to live,
-//assuming we live until 90yrs old
-// assuming a year = 12 months, 52 weeks and 365 days
+// assuming we'll live until 90yrs old
+// 1 year = 12 months, 52 weeks and 365 days
 // take as input: currentAge
-// display as console.log in the format:
+
+// display in console.log in the format:
 // "you've got x days, y weeks and z months left to live."
 // being x, y and z replaced with calculated numbers.
 
