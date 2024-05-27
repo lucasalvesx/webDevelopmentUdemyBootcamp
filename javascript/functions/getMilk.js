7fa46d4387035b9ad94b1b2f8dd0900c3fd40547
Lucas <lucassalves005@gmail.com>
2024-05-27 20:12:18 -0300

    27052024

--- javascript/functions/getMilk.js
+++ javascript/functions/getMilk.js
@@ -6,28 +6,7 @@
 // When you pass smthng as an argument in a function, it creates a var
 
 // same function as previous example:
-function getMilk(bottles) {
-    alert ("leaveHouse")
-    alert ("moveRight")
-    alert ("moveRight")
-    alert ("moveUp")
-    alert ("moveUp")
-    alert ("moveUp")
-    alert ("moveUp")
-    alert ("moveRight")
-    alert ("moveRight")
-    //adding var input, so when the function gets invoked, it executes with the user-selected value
-    alert ("buy" + bottles + "bottles of Milk")
-    alert ("moveLeft")
-    alert ("moveLeft")
-    alert ("moveDown")
-    alert ("moveDown")
-    alert ("moveDown")
-    alert ("moveDown")
-    alert ("moveLeft")
-    alert ("moveLeft")
-    alert ("enterHouse")
-}
+
 
 // so to invoke the function, we call:
 getMilk(2);
--- javascript/functions/returnValues.js
+++ javascript/functions/returnValues.js
@@ -1,6 +1,11 @@
 // all inside the () are called parameters
+// math.floor rounds down a broken number
 
-function getMilk(bottles) {
+// code is a problem to be solved: we need to program a bot that will buy us milk
+// we give it some amount of money, and based off how much it costs, it will bring us the affordable amount of milk
+// it should also calculate the change to be received, and bring it back to us.
+
+function getMilk(money, costPerBottle) {
     console.log ("leaveHouse")
     console.log ("moveRight")
     console.log ("moveRight")
@@ -10,7 +15,10 @@ function getMilk(bottles) {
     console.log ("moveUp")
     console.log ("moveRight")
     console.log ("moveRight")
-    console.log ("buy" + bottles + "bottles of Milk")
+
+    console.log( "buy " + calcBottles(money, costPerBottle) + " bottles of milk" );
+    // calcBottles function is defined below, 1.5 is how much it costs
+
     console.log ("moveLeft")
     console.log ("moveLeft")
     console.log ("moveDown")
@@ -20,16 +28,32 @@ function getMilk(bottles) {
     console.log ("moveLeft")
     console.log ("moveLeft")
     console.log ("enterHouse")
+
+//now we specify what we want as an output of this function
+
+    return money % 1.5; //will give us the remainder of division
 }
+
+// we can store the output of the function above inside a variable, here called change:
+var change = getMilk(); //inside the (parameters) we specify how many bottles we'd wish
+console.log(change); // outputs the result
 
 function calcBottles(startingMoney, costPerBottle) {
     numberOfBottles = Math.floor(startingMoney / costPerBottle);
-    return numberOfBottles
+    return numberOfBottles;
 }
 
+// now we'll calculate the change:
 function calcChange(startingAmount, costPerBottle) {
     var change = startingAmount % costPerBottle;
     return change;
 }
 
-console.log("Hey there sir, here's your $ " + getMilk(10, 3) + " change! :)");
+// so we call the function, saying $5 is the parameter - as we declared - for how much money we got
+// adn also the costPerBottle 
+console.log("Hey there sir, here's your $ " + getMilk(5, 1.5) + " change! :)");
+
+
+
+
+
