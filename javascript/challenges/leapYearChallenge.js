ae16898e5cf7050e15d7c74d32752758d748d77f
Lucas <lucassalves005@gmail.com>
2024-06-04 19:27:22 -0300

    04062024

--- javascript/challenges/leapYearChallenge.js
+++ javascript/challenges/leapYearChallenge.js
@@ -1,6 +1,7 @@
 // for a year to be a leap yer, we got the rules:
 // it must be evenly divisible by 4, by 100 and by 400
 // to check this we use the modulo operator - the rest must be 0
+
 function isLeap(year) {
   if (year % 4 === 0){
     if (year % 100 === 0){
