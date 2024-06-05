7bc27a67d91f042af9f544452ab2aa7d1672b694
Lucas <lucassalves005@gmail.com>
2024-06-04 22:22:47 -0300

    04062024

--- javascript/arrays/fizzBuzz.js
+++ javascript/arrays/fizzBuzz.js
@@ -1,40 +1,43 @@
-// fizzbuzz is a british kids game to teach division, which rules include:
+// Fizzbuzz is a british kids game to teach division, which rules include:
 // Players take turns to count incrementally, replacing any number divisible by 3 
-//with the word "fizz", and any number divisible by 5 with the word "buzz",
-//and any number divisible by both 3 and 5 with the word "fizzbuzz"
-// so... 1, 2, fizz, 4, buzz...
+// with the word "fizz", and any number divisible by 5 with the word "buzz",
+// and any number divisible by both 3 and 5 with the word "fizzbuzz"
+// so for example: 1, 2, fizz, 4, buzz...
 
-// i recommend myself to apply flowcharts and basic programming logics into this code
+// it's recommended to apply flowcharts and basic programming logics into this problem;
+// important to remember that conditionals are interpreted in a flow...
+// where all conditions are interpreted to be true or false, and if false, gets the next one to be tested and so on
 
 var output = [];//array to be included the incremented number when function is called
-var count = 1; //start counting from 1
+var count = 1; //starts counting from 1
 
 //function to be called on console
 function FizzBuzz() {
 
-    //if count number when divided by 3 is left with 0:
+    // first check if count is divisible by both 3 and 5
+    if (count % 3 === 0 && count % 5 === 0){
+        output.push("Fizzbuzz")
+    }
+    
+    //if not by both, check if count number is divisible by 3:
     if (count % 3 === 0) {
-        //...into the array, push the word "fizz"
-        output.push("Fizz") //because it's divisible by 3
-    } else {
-        //otherwise, if not divisible, keep on counting +1 
-        output.push(count);//push into the array, the incremented counting 
+        output.push("Fizz") //if so, push the word "fizz" into the array
     }
 
-    //if the count number is divisible by 5:
-    if (count % 5 === 0) {
-        output.push("Buzz")//...push the word "buzz" into the array
-    } else {
-        output.push(count)// if not, keep on counting
+    //if not by both or by 3, check if count number is divisible by 5:
+    else if (count % 5 === 0) {
+        output.push("Buzz")//if so, push the word "buzz" into the array
     }
 
-    // if the count number is divisible by both 3 and 5:
-    if (count % 3 === 0 && count % 3 === 0){
-        output.push("FizzBuzz")//then replace the count number with the word "FizzBuzz"
-    } else {
-        output.push(count)//otherwise keep on counting
-    }
+    //otherwise, if not divisible by any condition, keep on counting +1 
+    else {
+        output.push(count)
+    }  
 
     count++;//always incrementing +1   
-    console.log(output);//show array on console 
+    console.log(output);//show array on console
 }
+
+FizzBuzz();
+
+
