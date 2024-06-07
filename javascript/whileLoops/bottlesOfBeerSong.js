f215aeb8dbe017e19098df2c5294187bb0241c5a
Lucas <lucassalves005@gmail.com>
2024-06-06 23:58:21 -0300

    06062024

--- javascript/whileLoops/bottlesOfBeerSong.js
+++ javascript/whileLoops/bottlesOfBeerSong.js
@@ -1,19 +1,24 @@
 // automating the lyrics to a popular song which counts in decrement from 99 to 0
 // using functions and while loops
 
-var i = 99;
-var decrement = i--;
+var i = 99;//accounting variable - since we're decrementing, it starts in the highest number
 
-function song() {
-    while (i >= 0) {
+function song() {//declarating function
+    
+    while (i >= 0) {//more o equal to 0, meaning it stops counting on 0
+        
+        //print this on console
         console.log(i + " bottles of beer on the wall, " + i + " bottles of beer" +
-            "Take one down and pass it around, " + decrement + "bottles of beer on the wall."
+            "Take one down and pass it around, " + i + " bottles of beer on the wall."
         );
-    }
 
-    i--
+        //then decrements from variable (still inside the loop)
+        i--
+
+    }
 
 }
 
+//calls function
 song()
 
