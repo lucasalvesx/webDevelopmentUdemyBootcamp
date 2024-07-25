2e82b6a361121ab0a19c40af776d5ff5bced54ed
Lucas <lucassalves005@gmail.com>
2024-07-24 21:44:55 -0300

    240724

---
+++ jQuery/main.js
@@ -0,0 +1,8 @@
+//to start jquery we use the $ sign
+
+//to make sure that jquery waits until the browser reads it so it could be able to start,
+//we declare the ready function
+
+$(document).ready(function(){
+    $("h1").css("color", "red");
+})
--- javascript/methods/methods.js
+++ javascript/methods/methods.js
@@ -6,8 +6,20 @@ function Assistant (name, age, role, experienced) {
     this.age = age;
     this.role = role;
     this.experienced = experienced
+    //method example:
+    this.writeReport = function() {
+        alert("Tell me what you need: ");
+        makeReport();
+        deliverForReview();
+    }
+    this.clean = function() {
+        alert("Cleaning in progress... Hold on");
+    }
 }
 
+//creating new OBJECTS
 var assistant1 = new Assistant("Bruno", 25, "Aux. contabil", true)
 var assistant2 = new Assistant("Fernando", 22, "auxiliar de depilação", true)
 
+//calling a method for an object
+assistant1.writeReport();
