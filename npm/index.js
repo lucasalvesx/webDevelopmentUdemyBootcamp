0994d0362205593cd2ec512eec0b4f500f2b37e3
Lucas <lucassalves005@gmail.com>
2024-08-07 01:40:58 -0300

    npm and gitignore

---
+++ npm/index.js
@@ -0,0 +1,4 @@
+// we give the [npm init] command to initiate a new package
+// then its generated a JSON file containig info provided
+
+// then we install a preferred package with the [npm i package-name] command
